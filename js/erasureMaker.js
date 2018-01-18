6e4b0671ff44548f3b706fbfa2629fab374eb310
Alan Harnum <waharnum@gmail.com>
2018-01-18 18:38:39 -0500

    Initial commit of code from CodePen.

---
+++ js/erasureMaker.js
@@ -0,0 +1,209 @@
+var TEXT_BUTLER_WAR = {
+source: "https://archive.org/stream/WarIsARacket/WarIsARacket_djvu.txt",
+text:
+`
+The only way to smash this racket is to conscript capital and industry and labor before the
+nations manhood can be conscripted. One month before the Government can conscript the
+young men of the nation -- it must conscript capital and industry and labor. Let the officers
+and the directors and the high-powered executives of our armament factories and our
+munitions makers and our shipbuilders and our airplane builders and the manufacturers of all
+the other things that provide profit in war time as well as the bankers and the speculators, be
+conscripted -- to get $30 a month, the same wage as the lads in the trenches get.
+
+Let the workers in these plants get the same wages -- all the workers, all presidents, all
+executives, all directors, all managers, all bankers -- yes, and all generals and all admirals
+and all officers and all politicians and all government office holders -- everyone in the nation
+be restricted to a total monthly income not to exceed that paid to the soldier in the trenches!
+
+Let all these kings and tycoons and masters of business and all those workers in industry and
+all our senators and governors and majors pay half of their monthly $30 wage to their
+families and pay war risk insurance and buy Liberty Bonds.
+`
+}
+
+var TEXT_STOKES_MOTOR = {
+source: "https://www.gutenberg.org/files/35857/35857-0.txt",
+text:
+`
+It was quite dark when the train pulled into Paddington Station in
+London. It was raining, too, and the wet asphalt streets became mirrors
+underfoot, reflecting the myriad lights of the city. There was great
+confusion at the depot. Luggage must be identified and collected;
+steamer friends parted with and cabs engaged.
+
+“My goodness!” exclaimed Miss Campbell, who had not been in London for
+twenty years, “I feel so lonesome all of a sudden in this crush. I do
+wish we had a man to help us.”
+
+The wish was no sooner uttered than it was granted by the kind and
+merciful providence who has a special tenderness for helpless,
+middle-aged spinsters.
+`
+}
+
+var TEXT_BURROUGHS_CORE = {
+source: "http://www.gutenberg.org/cache/epub/123/pg123.txt",
+text:
+`
+Along the crowded avenue we marched, the guards showing unusual cruelty
+toward us, as though we, too, had been implicated in the murder of
+their fellow.  The occasion was to serve as an object-lesson to all
+other slaves of the danger and futility of attempted escape, and the
+fatal consequences of taking the life of a superior being, and so I
+imagine that Sagoths felt amply justified in making the entire
+proceeding as uncomfortable and painful to us as possible.
+
+They jabbed us with their spears and struck at us with the hatchets at
+the least provocation, and at no provocation at all.  It was a most
+uncomfortable half-hour that we spent before we were finally herded
+through a low entrance into a huge building the center of which was
+given up to a good-sized arena.  Benches surrounded this open space
+upon three sides, and along the fourth were heaped huge bowlders which
+rose in receding tiers toward the roof.
+
+At first I couldn't make out the purpose of this mighty pile of rock,
+unless it were intended as a rough and picturesque background for the
+scenes which were enacted in the arena before it, but presently, after
+the wooden benches had been pretty well filled by slaves and Sagoths, I
+discovered the purpose of the bowlders, for then the Mahars began to
+file into the enclosure.
+`
+}
+
+
+var TEXT_KROPOTKIN_CONQUEST = {
+source: "http://www.gutenberg.org/cache/epub/23428/pg23428.txt",
+text:
+`
+Education still remains the privilege of a small minority, for it is idle to talk of education when the workman's child is forced, at the age of thirteen, to go down into the mine or to help his father on the farm. It is idle to talk of studying to the worker, who comes home in the evening wearied by excessive toil, and its brutalizing atmosphere. Society is thus bound to remain divided into two hostile camps, and in such conditions freedom is a vain word. The Radical begins by demanding a greater extension of political rights, but he soon sees that the breath of liberty leads to the uplifting of the proletariat, and then he turns round, changes his opinions, and reverts to repressive legislation and government by the sword.
+
+A vast array of courts, judges, executioners, policemen, and gaolers is needed to uphold these privileges; and this array gives rise in its turn to a whole system of espionage, of false witness, of spies, of threats and corruption.
+
+The system under which we live checks in its turn the growth of the social sentiment. We all know that without uprightness, without self-respect, without sympathy and mutual aid, human kind must perish, as perish the few races of animals living by rapine, or the slave-keeping ants. But such ideas are not to the taste of the ruling classes, and they have elaborated a whole system of pseudo-science to teach the contrary.
+`
+}
+
+function getRandomInt(max) {
+return Math.floor(Math.random() * Math.floor(max));
+}
+
+var texts = [TEXT_BUTLER_WAR, TEXT_STOKES_MOTOR, TEXT_BURROUGHS_CORE, TEXT_KROPOTKIN_CONQUEST]
+
+var selectedText = texts[getRandomInt(texts.length)];
+
+var source = selectedText.source;
+
+var sourceMarkup = "<a href='" + source + "'>Source</a>";
+
+$(".source").append(sourceMarkup);
+
+var MODE_CLICK = "MODE_CLICK";
+var MODE_ERASE = "MODE_ERASE";
+var MODE_RESTORE = "MODE_RESTORE";
+
+var currentMode = MODE_CLICK;
+
+$(document).ready(function () {
+
+
+    var text = selectedText.text;
+
+    var splitText = text.split("\n\n");
+
+    splitText.forEach(function (para) {
+    var paraMarkup = "<p class='paragraph'>" + para + "</p>"
+    $(".text").append(paraMarkup);
+    });
+
+    $(".mode-control-click").click(function () {
+    currentMode = MODE_CLICK;
+    $(this).addClass("current-control");
+      $(".mode-control-erase").removeClass("current-control")
+        $(".mode-control-restore").removeClass("current-control");
+    });
+
+    $(".mode-control-erase").click(function () {
+      currentMode = MODE_ERASE;
+      $(this).addClass("current-control");
+      $(".mode-control-click").removeClass("current-control");
+    $(".mode-control-restore").removeClass("current-control");
+    });
+
+    $(".mode-control-restore").click(function () {
+      currentMode = MODE_RESTORE;
+      $(this).addClass("current-control");
+      $(".mode-control-click").removeClass("current-control");
+    $(".mode-control-erase").removeClass("current-control");
+    });
+
+    $(document).keyup(function (e) {
+    if(e.key === "c") {
+      $(".mode-control-click").click();
+    }
+    if(e.key === "e") {
+      $(".mode-control-erase").click();
+    }
+    if(e.key === "r") {
+      $(".mode-control-restore").click();
+    }
+    });
+
+    $(".function-control-remove").click(function () {
+    $(".fade").each(function() {
+      $(this).addClass("removed");
+    })
+    });
+
+    $(".function-control-restore").click(function () {
+    $(".character").each(function() {
+      $(this).removeClass("removed");
+    })
+    });
+
+    $(".function-control-erase-all").click(function () {
+    $(".character").each(function() {
+      $(this).addClass("fade");
+    })
+    });
+
+    $(".function-control-restore-all").click(function () {
+    $(".character").each(function() {
+      $(this).removeClass("fade");
+    })
+    });
+
+    $(".function-control-finalize").click(function () {
+    $(".fade").each(function() {
+      $(this).html("<span class='spacer'></span>");
+    })
+    });
+
+    $(".paragraph").each(function (i,e) {
+    var splitText = $(this).text().split("");
+
+    var spannedText = [];
+
+    splitText.forEach(function (currentValue) {
+      var spanned = "<span class='character'>" + currentValue + "</span>";
+      spannedText.push(spanned);
+    });
+
+    $(this).html(spannedText.join(""));
+    });
+
+    $(".character").each(function (i,e) {
+    $(this).click(function () {
+      if(currentMode === MODE_CLICK) {
+        $(this).toggleClass("fade");
+      }
+    });
+    $(this).mouseenter(function () {
+      if(currentMode === MODE_ERASE) {
+        $(this).addClass("fade");
+      }
+      if(currentMode === MODE_RESTORE) {
+        $(this).removeClass("fade");
+      }
+    });
+    });
+});
