78fb36ffad816fccc385313ca12f2477f74a6d79
Alan Harnum <waharnum@gmail.com>
2018-01-28 15:39:42 -0500

    Periodic indexing while server is running, and indexing on startup.

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ var ca = fluid.registerNamespace("ca");
 require("./server/serverSetup.js");
 require("./server/handlers.js");
 require("./server/dataSources.js");
+require("./server/index.js");
 
 var server = ca.alanharnum.erasuremaker.server();
---
+++ server/index.js
@@ -0,0 +1,55 @@
+"use strict";
+
+var fluid = require("infusion");
+var fs = require("fs");
+var indexingInterval = 300000;
+
+var index = function () {
+    console.log("Updating index files");
+    var storagePath = "./storage/erasures/";
+    var indexesPath = "./storage/indexes/"
+
+    var index = {};
+
+    fs.readdir(storagePath, function(err, files) {
+        files.forEach(function (filename) {
+            if(filename !== ".gitignore") {
+                var filePath = storagePath + filename;
+
+                var data = fs.readFileSync(filePath, "utf8");
+                var storedErasure = JSON.parse(data);
+                var erasureDetails = {
+                    sourceKey: storedErasure.sourceKey,
+                    erasureKey: filename,
+                    erasureTitle: storedErasure.title,
+                    sourceURL: storedErasure.sourceURL,
+                    sourceTextAuthor: storedErasure.sourceTextAuthor,
+                    sourceTextTitle: storedErasure.sourceTextTitle
+                };
+
+                var indexAt = index[erasureDetails.sourceKey] ? index[erasureDetails.sourceKey] : index[erasureDetails.sourceKey] = {};
+
+                indexAt[erasureDetails.erasureKey] = erasureDetails;
+
+            }
+        });
+
+        fluid.each(index, function (content, key) {
+            fs.writeFile(indexesPath + key, JSON.stringify(content), function (err) {
+                if(err) {
+                    console.log("Error updating index for " + key);
+                } else {
+                    console.log("Index file for " + key + " updated");
+                }
+            });
+        });
+    });
+};
+
+index();
+console.log("Indexing running every " + indexingInterval + " milliseconds");
+
+setInterval(function () {
+    console.log("Recurring index job running");
+    index();
+}, indexingInterval);
