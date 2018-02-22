3fbed4120399b156d4be7d3664515f1bb1c6684a
Alan Harnum <waharnum@gmail.com>
2018-02-22 16:46:29 -0500

    Colors in CSS variables, button styles.

--- client/js/erasureMaker.js
+++ client/js/erasureMaker.js
@@ -252,15 +252,15 @@ fluid.defaults("ca.alanharnum.erasureMaker.controls", {
         `
         <form>
             <div class="mode-controls controls">
-                <label class="mode-control-click current-control">
+                <label class="mode-control mode-control-click current-control">
                     <input class="mode-control-click-radio" checked type="radio" name="mode-control-radio" value="click" />
                     <span class="keyboard-shortcut-indicator">c</span>lickmode
                 </label>
-                <label class="mode-control-erase">
+                <label class="mode-control mode-control-erase">
                     <input class="mode-control-click-radio" type="radio" name="mode-control-radio" value="erase" />
                     <span class="keyboard-shortcut-indicator">e</span>rasemode
                 </label>
-                <label class="mode-control-restore">
+                <label class="mode-control mode-control-restore">
                     <input class="mode-control-click-radio" type="radio" name="mode-control-radio" value="restore" />
                     <span class="keyboard-shortcut-indicator">r</span>estoremode
                 </label>
