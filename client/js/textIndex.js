e8d5f5da347317d2ad116973c0362c525a0b670f
Alan Harnum <waharnum@gmail.com>
2018-03-23 14:50:37 -0400

    Add creation of new erasures from specific texts to index.

--- client/js/textIndex.js
+++ client/js/textIndex.js
@@ -43,43 +43,48 @@ fluid.defaults("ca.alanharnum.erasureMaker.textIndex", {
 });
 
 ca.alanharnum.erasureMaker.textIndex.generateMarkup = function (availableErasureTexts) {
-    var indexMarkup = "<h2>Erasures by Text Excerpt</h2>";
+    var indexMarkup = "<h2>Text Excerpts</h2>";
 
     var textsAsArray = fluid.hashToArray(availableErasureTexts.model.texts, "textKey");
 
 
     textsAsArray.sort(function (textA, textB) {
-        return textA.title > textB.title;
+        if(textA.title < textB.title) return -1;
+        if(textA.title > textB.title) return 1;
+        return 0;
     });
 
     fluid.each(textsAsArray, function (text, idx) {
         var textMarkup =
         `
-            <h3 data-textKey="${text.textKey}" class="index-item">${text.title}, ${text.author}</h3>
+            <h3 data-textKey="${text.textKey}" class="index-item">${text.title}, ${text.author} <a class="index-item-new-erasure" href="edit.html?textId=${text.textKey}">Create New Erasure from This Excerpt</a></h3>
         `
         indexMarkup = indexMarkup + textMarkup;
     })
     return indexMarkup;
 };
 
-ca.alanharnum.erasureMaker.textIndex.addIndexes = function (that) {    
+ca.alanharnum.erasureMaker.textIndex.addIndexes = function (that) {
     var indexItems = that.locate("index-item");
     fluid.each(indexItems, function (indexItem) {
         var indexItemDOM = $(indexItem);
         var textKey = indexItemDOM.attr("data-textKey");
+
         var indexURL = `http://${window.location.host}/indexes/${textKey}`;
         var jqxhr = $.get(indexURL, function () {})
             .done(function (data) {
                 var erasureIndex = JSON.parse(data);
-                indexItemDOM.after("<ol class='index-list'></ol>")
+                indexItemDOM.after("<ol class='index-list'></ol>");
                 fluid.each(erasureIndex, function (erasureIndexItem) {
                     var list = indexItemDOM.next("ol");
                     list.append(`<li class="index-list-item"><a href="view.html?erasureId=${erasureIndexItem.erasureKey}">${erasureIndexItem.erasureTitle}</a></li>`);
                 });
             })
             .fail(function (error) {
-                console.log(error);
-                indexItemDOM.remove();
+                indexItemDOM.after("<ul class='index-list'></ul>");
+                var list = indexItemDOM.next("ul");
+                list.append(`<li class="index-list-item">No erasures for this excerpt yet; maybe you should <a href="edit.html?textId=${textKey}">create one.</a></li>`);
+                // indexItemDOM.remove();
             });
     })
 };
